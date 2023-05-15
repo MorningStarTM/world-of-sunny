f8b57b456a219deff20008062ff02d166068a801
ErnestBeckham <ernestbeckham1998@gmail.com>
2023-05-15 15:48:39 +0530

    updated

--- world of sunny/Assets/Scripts/PlayerMovement.cs
+++ world of sunny/Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,22 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
     private Animator anim;
+    private BoxCollider2D coll;
 
     private enum MovementState { idle, running, jumping, falling}
     private MovementState state = MovementState.idle;
 
     private float dirX = 0f;
     private float jumpForce = 14f;
+
+    [SerializeField] private LayerMask jumpableGround;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -26,7 +30,7 @@ public class PlayerMovement : MonoBehaviour
     {
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -61,5 +65,10 @@ public class PlayerMovement : MonoBehaviour
             state = MovementState.falling;
         }
         anim.SetInteger("state", (int)state);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
